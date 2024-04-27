babc5d0dc940e13f489662bfedd85dc35d937de1
vineelvanjari24 <136819348+vineelvanjari24@users.noreply.github.com>
2024-04-27 17:01:07 +0000

    init

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-echo "hello world"
-echo "replit"
-echo "vs code"
+<html lang="en">
+<head>
+    <style>
+        @media (prefers-color-scheme: dark) {
+            body {
+                filter: invert(1) hue-rotate(180deg);
+                background-color: #000;
+                color: #000;
+            }
+        }
+    </style>
+</head>
+<body>
+  <script>
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        location.reload();
+      }
+    });
+  </script>
+  <?php 
+    $b = "<br>";
+    echo "printing in echo ".$b;
+    echo("printing in echo method".$b);
+    echo print "print  returns 1 value".$b;
+    print("printing in print method".$b);
+  ?>
+</body>
+</html>
